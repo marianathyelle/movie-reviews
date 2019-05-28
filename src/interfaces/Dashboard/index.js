b7d9eb4e4ca964aa9b1eb1c2233e20683117c01e
Maria Nathyelle <marianathyelle@nalaje.com>
2019-05-28 13:27:39 -0300

    created useReviews hook

--- src/interfaces/Dashboard/index.js
+++ src/interfaces/Dashboard/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Container, PageHeader } from './styles';
+import useReviews from '../../logic-hooks/useReviews';
 
-export const DashboardInterface = () => (
+export const DashboardInterface = () => {
+  const stateReviews = useReviews();
+  return(
     <Container>
       <PageHeader>
         <h1>Movie Reviews</h1>
         <p>Our film critics on blockbusters, independents and everything in between.</p>
       </PageHeader>
+      {stateReviews.reviews && stateReviews.reviews.map(item => (
+        <li key={item.display_title}>{item.display_title}</li>
+      ))}
     </Container>
-);
+  )
+}
---
+++ src/logic-hooks/useReviews.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { API_URL, params } from '../services';
+
+export default function useReviews() {
+  const [state, setState] = useState({
+    isLoading: false, 
+    reviews: [],
+    error: false
+  });
+
+  async function getReviews() {
+    setState({ isLoading: true })
+    try {
+      const response = await fetch(`${API_URL}/reviews/all.json?order=${params.order}&api-key=${params["api-key"]}`)
+      const data = await response.json();
+      setState({ isLoading: false, reviews: data.results });
+    } catch(error) {
+      setState({ isLoading: false, error: true })
+    }
+  }
+
+  useEffect(() => {
+    getReviews();
+    return () => void 0;
+  }, []);
+
+  return state;
+}
